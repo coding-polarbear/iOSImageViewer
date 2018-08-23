051f85f27a6a5e53ed9cf7fa85d0625bef1433ae
배현빈 <baehynebin@hotmail.co.kr>
2018-08-23 15:30:43 +0900

    Initialize Commit

--- education1/ViewController.swift
+++ education1/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    struct Keys {
+        static let index = "index"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var imageIndex : Int = 1 {
+        didSet {
+            label.text = "\(imageIndex) / 5"
+            imageView.image = UIImage(named : "Cat\(imageIndex)")
+            prevButton.isEnabled = imageIndex > 1
+            nextButton.isEnabled = imageIndex < 5
+        }
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let idx = UserDefaults.standard.integer(forKey: Keys.index)
+        print("idx = \(idx)")
+        if 1 <= idx && idx <= 5 {
+            imageIndex = idx
+        } else {
+            imageIndex = 1
+        }
+    }
+    
+    func updateIndex() {
+        label.text = "\(imageIndex) / 5"
+        imageView.image = UIImage(named : "Cat\(imageIndex)")
+        UserDefaults.standard.set(imageIndex, forKey: Keys.index)
+    }
+    
+    
+    @IBAction func onButtonPrev(_ sender: UIButton) {
+        if(imageIndex <= 5 && imageIndex > 1) {
+            imageIndex -= 1
+            updateIndex()
+        }
+    }
+    @IBAction func onButtonNext(_ sender: UIButton) {
+        if(imageIndex >= 1 && imageIndex <= 4) {
+            imageIndex += 1
+            updateIndex()
+        }
+    }
 }
 
